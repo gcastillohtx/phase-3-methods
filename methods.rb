f6f12d7536411e99219df5a68ada3ce692012cbe
Gil <gcastillohtx@gmail.com>
2022-02-22 22:42:37 -0600

    commit tree 67a00abe08a852bb9089da2ec93a5579b7f128f4

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
-# Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end 
+
+def greet (name)
+    puts "Hello, #{name}!"
+end 
+
+greet "Naureen"
+
+def greet_with_default (name="programmer")
+    puts "Hello, #{name}!"
+end 
+
+greet_with_default
+
+def add (num1, num2)
+    return num1 + num2
+end
+
+add(1,2)
+
+def halve (num1, num2)
+    puts num1 / num2
+    return num1 / num2
+end 
+
+halve num1 / "six"
+halve(6,3)
